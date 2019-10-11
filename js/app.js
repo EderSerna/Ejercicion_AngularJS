a56e0c6e44b8128a18d59c9b7f85e07080830928
EderSerna <serna.eder74@gmail.com>
2019-10-11 13:54:56 +0200

    qqqqqqqqqqq

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 var app = angular.module('tareasApp',[]);
+app.controller('mainCtrl',['$scope','$http',functio($scope,$http){
+
+this.$onInit=function(){
+    console.log('oninit mainctrl');
+
+    let url='http://localhost:3000:frutas';
+    $http.get
+}
+//
+
+
+
+}]);
